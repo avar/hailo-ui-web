dbf61e8add8f373381d8403dbf87010f99824cd9
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-02-18 19:57:30 +0000

    Code cleanup

--- root/static/js/chat.js
+++ root/static/js/chat.js
@@ -1,44 +1,27 @@
-/* 
-Created by: Kenrick Beckett
-
-Name: Chat Engine
-*/
-
-var instanse = false;
-var state;
-var mes;
-var file;
-
 function Chat () {
-    this.update = function() {};
     this.send = sendChat;
-	this.getState = function() {};
 }
-
 
 //send the message
 function sendChat(message, nickname)
 {       
-
     add_to_chat(nickname, message);
 
-     $.ajax({
-		   type: "GET",
-           url: "/chat/rest/reply",
-		   data: {  
-                'nickname': nickname,
-                 'message': message,
-       	 },
-		   dataType: "json",
-		   success: function(data){
-				   if(data.text){
-                       $('#chat-area').append($("<p>" + "<span>" + data.nick + "</span>" + data.text +"</p>"));
-                       document.getElementById('chat-area').scrollTop = document.getElementById('chat-area').scrollHeight;
-                   }
-		   },
-		});
+    $.ajax({
+        type: "GET",
+        url: "/chat/rest/reply",
+        data: {  
+            'nickname': nickname,
+            'message': message,
+        },
+        dataType: "json",
+        success: function(data){
+            if (data.nick && data.text) {
+                add_to_chat(data.nick, data.text);
+            }
+        },
+    });
 }
-
 
 function add_to_chat(nick, text) {
     $('#chat-area').append($("<p>" + "<span>" + nick + "</span>" + text +"</p>"));
